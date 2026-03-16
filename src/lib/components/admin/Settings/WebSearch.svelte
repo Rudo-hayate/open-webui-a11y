978eb69c525c2d3e55a27e5541c52fc553a64787
Guhl, Christopher <Christopher.Guhl@adesso.de>
2026-03-16 14:50:50 +0100

    Feat(a11y): add headings to support headingsmap in admin/websearch - correct label

--- src/lib/components/admin/Settings/WebSearch.svelte
+++ src/lib/components/admin/Settings/WebSearch.svelte
@@ -137,7 +137,7 @@
 		{#if webConfig}
 			<div class="">
 				<div class="mb-3">
-					<div class=" mt-0.5 mb-2.5 text-base font-medium">{$i18n.t('General')}</div>
+					<h1 class=" mt-0.5 mb-2.5 text-base font-medium">{$i18n.t('Web Search')}</h1>
 
 					<hr class=" border-gray-100/30 dark:border-gray-850/30 my-2" />
 
@@ -961,7 +961,7 @@
 				</div>
 
 				<div class="mb-3">
-					<div class=" mt-0.5 mb-2.5 text-base font-medium">{$i18n.t('Loader')}</div>
+					<h2 class=" mt-0.5 mb-2.5 text-base font-medium">{$i18n.t('Loader')}</h2>
 
 					<hr class=" border-gray-100/30 dark:border-gray-850/30 my-2" />
 
