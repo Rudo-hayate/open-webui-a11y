ba44963dc59ea0dceebf28870cf68c50d94e8dae
Guhl, Christopher <Christopher.Guhl@adesso.de>
2026-03-26 13:49:00 +0100

    Feat(a11y): Sidebar Elements #24 - increase contrast of switches in high contrast mode

--- src/lib/components/common/Switch.svelte
+++ src/lib/components/common/Switch.svelte
@@ -30,10 +30,10 @@
 		aria-labelledby={ariaLabelledbyId}
 		class="flex h-[1.125rem] min-h-[1.125rem] w-8 shrink-0 cursor-pointer items-center rounded-full px-1 mx-[1px] transition  {($settings?.highContrastMode ??
 		false)
-			? 'focus:outline focus:outline-2 focus:outline-gray-800 focus:dark:outline-gray-200'
+			? 'outline outline-2 outline-black dark:outline-white focus:outline focus:outline-4 focus:outline-black focus:dark:outline-white'
 			: 'outline outline-1 outline-gray-100 dark:outline-gray-800'} {state
-			? ' bg-emerald-500 dark:bg-emerald-700'
-			: 'bg-gray-200 dark:bg-transparent'}"
+			? ($settings?.highContrastMode ?? false) ? 'bg-green-700 dark:bg-green-400' : ' bg-emerald-500 dark:bg-emerald-700'
+			: ($settings?.highContrastMode ?? false) ? 'bg-gray-600 dark:bg-gray-600' : 'bg-gray-200 dark:bg-transparent'}"
 		onCheckedChange={async () => {
 			await tick();
 			dispatch('change', state);
