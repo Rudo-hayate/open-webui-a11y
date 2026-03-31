82e8e8cbbeaf859bfa8b579589f6b6b253b3a061
Guhl, Christopher <Christopher.Guhl@adesso.de>
2026-03-31 11:12:06 +0200

    Feat(a11y): Sidebar Elements #24 - increase contrast of badge colors (admin-panel)

--- src/lib/components/common/Badge.svelte
+++ src/lib/components/common/Badge.svelte
@@ -3,11 +3,11 @@
 	export let content = '';
 
 	const classNames: Record<string, string> = {
-		info: 'bg-blue-500/20 text-blue-700 dark:text-blue-200 ',
-		success: 'bg-green-500/20 text-green-700 dark:text-green-200',
-		warning: 'bg-yellow-500/20 text-yellow-700 dark:text-yellow-200',
-		error: 'bg-red-500/20 text-red-700 dark:text-red-200',
-		muted: 'bg-gray-500/20 text-gray-700 dark:text-gray-200'
+		info: 'bg-blue-500/20 text-blue-900 dark:bg-blue-900/40 dark:text-blue-100',
+		success: 'bg-green-600/20 text-green-900 dark:bg-green-900/40 dark:text-green-100',
+		warning: 'bg-yellow-500/20 text-yellow-900 dark:bg-yellow-900/40 dark:text-yellow-100',
+		error: 'bg-red-500/20 text-red-900 dark:bg-red-900/40 dark:text-red-100',
+		muted: 'bg-gray-500/30 text-gray-800 dark:text-gray-100'
 	};
 </script>
 
